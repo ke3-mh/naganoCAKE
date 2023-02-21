2657cbf092f5e739710c35169331b17f83a45f9e
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-02-21 02:42:26 +0000

    [Update]admin/customers会員へんｓｈへんｓｈu

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,8 +8,26 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer.id)
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:email,
+                                     :last_name,
+                                     :last_name_kana,
+                                     :first_name,
+                                     :first_name_kana,
+                                     :postal_code,
+                                     :address,
+                                     :telephone_number,
+                                     :is_deleted)
   end
 end
