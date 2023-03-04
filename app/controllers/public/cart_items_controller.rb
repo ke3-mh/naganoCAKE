f8142e396af3eeaa75262f32655e8fffda4f623b
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-03-04 07:21:30 +0000

    [Update]public_カート内商品一覧画面機能完了

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -14,8 +14,15 @@ class Public::CartItemsController < ApplicationController
     cart_item = CartItem.new(cart_item_params)
     cart_item.customer_id = current_customer.id
     cart_item.item_id = cart_item_params[:item_id]
-    cart_item.save
-    redirect_to cart_items_path
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
+      cart_item.save
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
