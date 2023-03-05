13e98fde162ee860b683913e3068d1811a2544a5
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-03-05 05:17:21 +0000

    [Update]public_注文情報確認画面機能実装　送料、商品合計、請求金額以外

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,17 +4,45 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
+
+    if params[:order][:select_address] == "1"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+
+    ## 登録済住所実装したときは以下に変更する
+    # elsif [:order][:select_address] == "2"
+    #   @order = Order.new(order_params)
+    #   @address = Address.find(params[:order][:address_id])
+    #   @order.postal_code = @address.postal_code
+    #   @order.address = @address.address
+    #   @order.name = @address.name
+
+    elsif params[:order][:select_address] == "3"
+      @order.customer_id = current_customer.id
+    end
+    
+    @cart_items = current_customer.cart_items
+    @order_new = Order.new
+    render :confirm
   end
 
   def complete
   end
 
   def create
+
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
