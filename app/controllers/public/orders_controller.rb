6673a3c9758c01a2ede9310c69bc5e5267ad7925
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-03-08 15:20:16 +0000

    [Update]public_商品履歴詳細画面機能実装完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -60,6 +60,7 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: @order.id)
   end
 
   private
